0eb3b5f0168efe91541895c3052e3c216d86c9df
krishna9809 <multiatoms980996@gmail.com>
2024-07-15 07:28:18 +0530

    Day-5

---
+++ Day-005/hasmap.cpp
@@ -0,0 +1,30 @@
+// Hashing:the definition of hashing in a naive way, it is nothing but the combination of the steps, pre-storing, and fetching.
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+
+    int n;
+    cin >> n;
+    int arr[n];
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    //precompute:
+    int hash[13] = {0};
+    for (int i = 0; i < n; i++) {
+        hash[arr[i]] += 1;
+    }
+
+    int q;
+    cin >> q;
+    while (q--) {
+        int number;
+        cin >> number;
+        // fetching:
+        cout << hash[number] << endl;
+    }
+    return 0;
+}
